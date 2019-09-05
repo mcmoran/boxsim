4014a444a613097aacd32c519ef1f49111e6ff38
Mark Moran <markmoran@gmail.com>
2019-09-05 12:58:11 -1000

    Current version

--- main.lua
+++ main.lua
@@ -4,14 +4,14 @@ function love.load()
     playingAreaWidth = 660
     playingAreaHeight = 480
 
-    -- setting color values
+    -- setting color values for box
     colorWhite = {1, 1, 1}
     colorBlack = {0, 0, 0}
     color1 = {.5, .5, .5} -- sleep
     color2 = {1, .4, .4} -- eat
     color3 = {.22, .67, 1} -- drink
-    color4 = {1, .65, 1} -- learn
-    color5 = {1, .9, 1} -- play
+    color4 = {1, .73, .48} -- learn
+    color5 = {1, .95, .75} -- play
     color6 = {.25, .9, .29} -- move
 
     -- create the box
@@ -25,6 +25,7 @@ function love.load()
     currentLocation = 1 -- where the box starts from
         currentRow = 1 -- 1 or 2
         currentCol = 1 -- 1, 2 or 3
+    needLocation = 1
 
 end
 
@@ -32,13 +33,26 @@ function love.update(dt) -- dt = delta time
 
     -- setting a conditional loop for keystroke actions
     if love.keyboard.isDown('up') then
-        boxY = boxY - 150 * dt -- use dt so it doesn't go too fast/slow
+        boxY = boxY - 250 * dt -- use dt so it doesn't go too fast/slow
     elseif love.keyboard.isDown('down') then
-        boxY = boxY + 150 * dt
+        boxY = boxY + 250 * dt
     elseif love.keyboard.isDown('left') then
-        boxX = boxX - 150 * dt
+        boxX = boxX - 250 * dt
     elseif love.keyboard.isDown('right') then
-        boxX = boxX + 150 * dt
+        boxX = boxX + 250 * dt
+    end
+
+    -- make it so the box doesn't go outside the playing area
+    if boxX < 0 then
+        boxX = 0
+    elseif boxX > 610 then
+        boxX = 610
+    end
+
+    if boxY < 80 then
+        boxY = 80
+    elseif boxY > 430 then
+        boxY = 430
     end
 
     -- this will determine if the box is completely in a location
@@ -49,6 +63,21 @@ function love.update(dt) -- dt = delta time
         elseif boxX > 220 and boxX < 390 then currentCol = 2
         elseif boxX < 170 then currentCol = 1
     end
+
+    -- if the box is in the right area, then change the color
+
+    if needLocation == currentLocation then
+        needLocation = love.math.random(1,6)
+    end
+
+    if needLocation == 1 then currentColor = color1
+    elseif needLocation == 2 then currentColor = color2
+    elseif needLocation == 3 then currentColor = color3
+    elseif needLocation == 4 then currentColor = color4
+    elseif needLocation == 5 then currentColor = color5
+    elseif needLocation == 6 then currentColor = color6
+    end
+    
 
     -- set the current location and color of the box
     if (currentRow == 1 and currentCol == 1) then
@@ -104,7 +133,6 @@ function love.draw()
     -- move box
     love.graphics.setColor(.19, .8, .19, 1)
     love.graphics.rectangle('fill', 440, 280, 220, 200)
-
 
     -- this is the boxsim
 
