bd293e6d8dd3adef497da7833cf90bf6d4ce9a1e
Mark Moran <markmoran@gmail.com>
2019-09-11 12:05:22 -1000

    Finalized 10 levels of colors (I hope)

--- main.lua
+++ main.lua
@@ -38,35 +38,95 @@ function love.load()
   --the player stored as a table. you can set these to taste.
   -- player = {x = 4, y = 4, size = TILE_SIZE, speed = 0.2, timer = 0}
 
-  colorLevel1 = { {1, 1, 1},
+-- level 1 = full colors
+  colorLevel1 = { {1, 1, 0},
+                  {0, 1, 0},
+                  {0, 1, 1},
+                  {1, 0, 1},
+                  {1, 0, 0},
+                  {1, 0.5, 0} }
+
+-- level 2 = bright rainbow
+  colorLevel2 = { {.96, .90, .23},
+                  {.28, .95, .2},
+                  {.21, .69, 1},
+                  {.67, .26, .8},
+                  {1, .23, .16},
+                  {.98, .54, .09} }
+
+-- level 3 = pastelle rainbow
+  colorLevel3 = { {.96, .91, .47},
+                  {.51, .96, .55},
+                  {.48, .65, .87},
+                  {.71, .47, .8},
+                  {1, .51, .43},
+                  {1, .71, .53} }
+
+--[[ level 3 = light rainbow
+  colorLevel3 = { {.96, .92, .72},
+                  {.73, .96, .78},
+                  {.69, .77, .87},
+                  {.75, .65, .82},
+                  {.99, .82, .79},
+                  {.98, .85, .73} }
+]]--
+-- level 4 = greyscale
+  colorLevel4 = { {1, 1, 1},
                   {.8, .8, .8},
                   {.6, .6, .6},
                   {.4, .4, .4},
                   {.2, .2, .2},
                   {0, 0, 0} }
 
-  colorLevel2 = { {.96, .94, .8},
+ -- level 5 = yellows
+  colorLevel5 = { {.96, .94, .8},
                   {.94, .86, .54},
                   {.86, .76, .32},
                   {.82, .7, .47},
                   {.72, .6, .02},
                   {.50, .4, 0} }
 
-  colorLevel3 = { {1, 1, 1},
-                  {.8, .8, .8},
-                  {.6, .6, .6},
-                  {.4, .4, .4},
-                  {.2, .2, .2},
-                  {0, 0, 0} }
+ -- level 6 = greens
+  colorLevel6 = { {.86, .96, .81},
+                  {.67, .94, .61},
+                  {.46, .86, .37},
+                  {.25, .82, .5},
+                  {.15, .74, .4},
+                  {.1, .6, .2} }
 
-    levelMap = { colorLevel1, colorLevel2, colorLevel3 }
+ -- level 7 = blues
+  colorLevel7 = { {.81, .9, .96},
+                  {.59, .8, .94},
+                  {.37, .69, .86},
+                  {.17, .61, .82},
+                  {0, .49, .74},
+                  {0, .3, .6} }
 
-    --[[colorLevel21 = {1, .9, .2}
-    colorLevel22 = {.9, .7, .1}
-    colorLevel23 = {.8, .6, .1}
-    colorLevel24 = {.7, .5, 0}
-    colorLevel25 = {.6, .4, 0}
-    colorLevel26 = {.5, .3, 0} ]]--
+ -- level 8 = purples
+  colorLevel8 = { {.88, .83, .96},
+                  {.76, .61, .94},
+                  {.57, .36, .86},
+                  {.43, .17, .82},
+                  {.3, 0, .74},
+                  {.2, 0, .5} }
+
+ -- level 9 = reds
+  colorLevel9 = { {.93, .95, .85},
+                  {.94, .61, .63},
+                  {.85, .36, .38},
+                  {.82, .18, .22},
+                  {.6, 0, .05},
+                  {.5, 0, 0} }
+
+ -- level 10 = oranges
+  colorLevel10 = { {.95, .86, .81},
+                  {.92, .73, .58},
+                  {.87, .58, .34},
+                  {.78, .43, .15},
+                  {.74, .32, 0},
+                  {.65, .22, 0} }
+
+    levelMap = { colorLevel1, colorLevel2, colorLevel3, colorLevel4, colorLevel5, colorLevel6, colorLevel7, colorLevel8, colorLevel9, colorLevel10 }
 
     color1 = {.5, .5, .5} -- sleep (grey)
     color2 = {1, .4, .4} -- eat (red)
